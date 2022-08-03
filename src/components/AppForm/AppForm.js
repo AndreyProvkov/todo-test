2a216d3e84ccb5763012de531832c6e69c6e09b1
Andrey Provkov <provkovasu15@gmail.com>
2022-08-03 21:53:11 +0500

    Add conditional rendering in AppForm

--- src/components/AppForm/AppForm.js
+++ src/components/AppForm/AppForm.js
@@ -2,11 +2,55 @@ import React, { Component } from 'react'
 import './AppForm.scss'
 
 export default class AppForm extends Component {
+    constructor(prop) {
+        super(prop)
+        // Объявляем новое состояние, в котором будем хранить текущее состояние приложения (добавление, редактирование или просмотр задачи) и значения текстовых полей
+        this.state = {
+            appStatus: 'watch',
+            taskTitle: '',
+            taskDescription: ''
+        }
+        this.changeAppStatus = this.changeAppStatus.bind(this)
+    }
+
     render() {
+        // Установим начальные значения для рендера
         const task = this.props.task
+        const appStatus = this.state.appStatus
+        let buttonText = (appStatus === 'add') ? 'Добавить' : 'Сохранить'
+        let title = 'Просмотр'
+        let taskName = <input type='text' placeholder='Название задачи' name='taskTitle' value={this.state.taskTitle} onChange={this.handleChange} />
+        let taskDescription = <textarea placeholder='Описание задачи' name='taskDescription' value={this.state.taskDescription} onChange={this.handleChange} />
+        let btn = (<div>
+            <button onClick={(e) => this.changeAppStatus(e, 'watch')}>Отмена</button>
+            <button>{buttonText}</button>
+        </div>)
+
+        // Проверяем состояние приложения, в зависимости от условия присваиваем переменным подходящие значения
+        switch (appStatus) {
+            case 'add': {
+                title = 'Добавление'
+                buttonText = 'Добавить'
+                break;
+            }
+            case 'edit': {
+                title = 'Редактирование'
+                break;
+            }
+            default: {
+                taskName = (<p>
+                    {task.title}
+                </p>)
+                taskDescription = (<p>
+                    {task.description}
+                </p>)
+                btn = <button type='text' onClick={(e) => this.changeAppStatus(e, 'add')}>+</button>
+                break;
+            }
+        }
 
         return (
-            <div className='app-form'>
+            <form className='app-form'>
                 {
                     // Используем условный рендеринг в зависимости от существования задачи
                     !task ?
@@ -15,23 +59,43 @@ export default class AppForm extends Component {
                         </h2> :
                         <div className='app-form__is-task'>
                             <h2 className='app-form__title'>
-                                Просмотр задачи
+                                {title} задачи
                             </h2>
                             <h3 className='app-form__subtitle'>
                                 Название задачи
                             </h3>
-                            <p>
-                                {task.title}
-                            </p>
+                            {taskName}
                             <h3 className='app-form__subtitle'>
                                 Описание задачи
                             </h3>
-                            <p>
-                                {task.description}
-                            </p>
+                            {taskDescription}
                         </div>
                 }
-            </div>
+                {btn}
+            </form>
         )
+    }
+
+    // Меняем статус приложения в зависимости от нажатой кнопки
+    changeAppStatus(e, status) {
+        e.preventDefault()
+        this.setState({ appStatus: status })
+
+        // В зависимости от текущего состояния приложения заполняем поля ввода
+        if (status === 'edit') {
+            this.setState({
+                taskTitle: this.props.task.title,
+                taskDescription: this.props.task.description
+            })
+        } else {
+            this.setState({
+                taskTitle: '',
+                taskDescription: ''
+            })
+        }
+    }
+
+    handleChange(e) {
+        this.setState({ [e.target.name]: e.target.value })
     }
 }
