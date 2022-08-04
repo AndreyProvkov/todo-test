e21dd78c759405d5cf863359010485b01b9dd097
Andrey Provkov <provkovasu15@gmail.com>
2022-08-04 17:06:24 +0500

    Add filter for task name

--- src/components/AppList/AppList.js
+++ src/components/AppList/AppList.js
@@ -3,20 +3,42 @@ import TaskList from '../TaskList/TaskList'
 import './AppList.scss'
 
 export default class AppList extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            value: ''
+        }
+        this.searchFilter = this.searchFilter.bind(this)
+        this.getFilterTasks = this.getFilterTasks.bind(this)
+    }
+
     render() {
         return (
             <div className='app-list'>
                 <h2 className='app-list__title'>
                     Задачи
                 </h2>
-                <input type='text' placeholder='Поиск задач' />
+                <input type='text' placeholder='Поиск задач' value={this.state.value} onInput={this.searchFilter} />
                 <TaskList
                     // Прокидываем далее в компонент свойства и методы
-                    tasks={this.props.tasks}
+                    tasks={this.getFilterTasks()}
                     selectedTask={this.props.selectedTask}
                     setSelectedTask={this.props.setSelectedTask}
                 />
             </div>
         )
+    }
+
+    searchFilter(e) {
+        this.setState({ value: e.target.value.toLowerCase() })
+    }
+
+    getFilterTasks() {
+        return this.props.tasks.filter(task => {
+            if (task.title.toLowerCase().includes(this.state.value)) {
+                return true
+            }
+            return false
+        })
     }
 }
